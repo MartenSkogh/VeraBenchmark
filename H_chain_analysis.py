b91fd19e494e175094219705474ab14c1e23faa0
Mårten Skogh <marten.skogh@gmail.com>
2020-03-23 10:13:45 +0100

    Removed parsing from this and moved to separate file

--- H_chain_analysis.py
+++ H_chain_analysis.py
@@ -9,56 +9,8 @@ settings_filename = 'H_chain_benchmarks.csv'
 
 
 import matplotlib.pyplot as plt
+from qiskit_debug_parser import get_mean_time, get_real_time, get_qubits 
 
-
-def mean_time_slurm_file(input_filename):
-    # we want to find the files where the energy evaluation is printed
-    data = []
-    with open(input_filename) as slurm_file:
-        for line in slurm_file:
-            if ' Energy evaluation ' in line:
-                date_time = datetime.fromisoformat(line.split(',', 1)[0])
-                idx = int(line.split(' Energy evaluation ')[1].split(' returned')[0])
-                energy = float(line.split(' returned ')[1].strip())
-                data.append((idx, energy, date_time))
-
-
-    total_time = data[-1][2] - data[0][2]
-    mean_time = total_time.total_seconds() / (len(data) - 1)
-
-    total_square = 0
-    prev = None
-    for d in data:
-        if prev == None:
-            prev = d[2]
-        else:
-            time = (d[2] - prev).total_seconds()
-            total_square += time ** 2
-            prev = d[2]
-
-    variance = total_square / (len(data) - 1) - (mean_time ** 2)
-    std_dev = np.sqrt(variance)
-
-    return mean_time, std_dev
-
-
-def real_time_slurm_file(input_filename):
-    # we want to find the files where the final 'real (wall) time' is printed
-    real_time = 0
-    with open(input_filename) as slurm_file:
-        for line in slurm_file:
-            if 'real\t' in line:
-                print(line)
-                time_str = line.strip().split('\t', 1)[1]
-                time_arr = time_str.split('m') # Split on the m
-                minutes = float(time_arr[0])
-                seconds = float(time_arr[1][:-1]) # Last char is 's'
-                real_time = minutes * 60 + seconds
-        if real_time == 0:
-            real_time = None
-            print('Did not finish')
-
-    return real_time
 
 
 # Read the settings
@@ -78,8 +30,8 @@ for setting in settings:
     jobid = setting[-1]
     
     try:
-        mean_time, std_dev = mean_time_slurm_file(data_dir + 'slurm-' + str(jobid) + '.out')
-        real_time = real_time_slurm_file(data_dir + 'slurm-' + str(jobid) + '.out')
+        mean_time, std_dev = get_mean_time(data_dir + 'slurm-' + str(jobid) + '.out')
+        real_time = get_real_time(data_dir + 'slurm-' + str(jobid) + '.out')
         mean_times.append(mean_time)
         mean_time_std_devs.append(std_dev)
         real_times.append(real_time)
@@ -90,7 +42,9 @@ for setting in settings:
         mean_time_std_devs.append(None)
         print(f'Could not find slurm file for jobid: {jobid}')
 
-
+for i in range(1,9):
+    nbr_qubits = get_qubits(data_dir + f'circuit_nH={i}.out')
+    print(f'There are {nbr_qubits} qubits in here!')
 
 # Plot mean evaluation time
 plt.figure(1)
